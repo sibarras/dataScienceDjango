27831fc6b688f03cc0bfb32c5a1a103c75e5c016
Samuel Ibarra <ing.samuelibarra@gmail.com>
2020-11-27 21:05:31 -0500

    cap 3

--- src/products/models.py
+++ src/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Product(models.Model):
@@ -8,4 +9,21 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(blank=True)
+    salesman = models.ForeignKey(User, on_delete=models.CASCADE)
+    # change required
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def save(self, *args, **kwargs):
+        self.total_price = self.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "Solled {} - {} items for {}".format(self.product.name, self.quantity, self.total_price)
     
