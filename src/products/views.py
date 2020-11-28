632c814e19ef75c481e6aaa27976e90fdcdabd5f
Samuel Ibarra <ing.samuelibarra@gmail.com>
2020-11-27 22:38:30 -0500

    cap 4

--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Product
+import pandas as pd
 # Create your views here.
+
+def chart_select_view(request):
+    qs = Product.objects.all().values()
+    
+    return render(request, 'products/main.html', {})
