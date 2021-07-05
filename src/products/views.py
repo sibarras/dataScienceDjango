446fa0c5c616777c6cbd37eafac3025849cbc0d3
Samuel Ibarra <ing.samuelibarra@gmail.com>
2021-07-05 15:18:45 -0500

    adding views

--- src/products/views.py
+++ src/products/views.py
@@ -3,7 +3,8 @@ from .models import Product
 import pandas as pd
 # Create your views here.
 
+
 def chart_select_view(request):
-    qs = Product.objects.all().values()
-    
+    product_df = pd.DataFrame(Product.objects.all().values())
+    print(product_df)
     return render(request, 'products/main.html', {})
